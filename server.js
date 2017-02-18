42434a2e7e16185b67ead055f55fef0fafdbeec5
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-02-17 19:31:45 -0800

    remove morgan

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const path = require('path')
 const cors = require('cors')
 
 app.options('*', cors())
-app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(path.join(__dirname, './client')))
+app.use(express.static(path.join(__dirname, 'client')))
 
 
 app.listen(app.get('port'), function() {
