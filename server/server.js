96e5759d94e854229904ef5b5ff5dd76bbc00bc4
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-02-18 10:12:43 -0800

    file structuring

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const routes = require('./api/routes.main');
+const hooks = require('./api/hooks')
+//const routes = require('./api/routes.main');
 
 app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/api', routes);
+//app.use('/api', routes);
+app.post('/api/teachable-hook', hooks.teachable);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, '../client')));
