ac1b79d99ad631e982bbb802e7c9eb9e0cab077b
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-02-20 19:13:45 -0800

    model controller

--- server/api/model.js
+++ server/api/model.js
@@ -2,12 +2,18 @@ const Sequelize = require('sequelize');
 const db = require('./db.config.js');
 
 const LectureCompletion = db.define('completion', {
-	"course_id"
-	"lecture_id"
-	"school_id"
-	"user_id"
-	"user_role"
-	"user_email"
-	
-
+	User_Name: Sequelize.STRING,
+	User_Email: Sequelize.STRING,
+	User_id: Sequelize.INTEGER,
+	Lecture_ID: Sequelize.INTEGER,
+	Lecture_Completion_Date: Sequelize.DATE,
+	Sign_In_Count: Sequelize.INTEGER,
+	Last_Sign_In: Sequelize.DATE
 })
+
+
+LectureCompletion.sync().then(() => {
+  console.log('Review table successfully created.')
+})
+
+module.exports = LectureCompletion
