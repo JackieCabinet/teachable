c5e51716913435aea21fbd386eda348f014d4471
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-02-22 11:47:23 -0800

    model/controller

--- server/api/db.config.js
+++ server/api/db.config.js
@@ -1,21 +1,21 @@
-// const Sequelize = require('sequelize')
+const Sequelize = require('sequelize')
 
-// // Please create a credential.json file with your username and password for your cloud DB.
-// // Refer to sampleCredentials.json
-// // const creds = require('./credentials')
+// Please create a credential.json file with your username and password for your cloud DB.
+// Refer to sampleCredentials.json
+// const creds = require('./credentials')
 
-// // Database connection
-// const DBConnection = new Sequelize('postgres://dtsjjtzz:LQWdLNzmw7RTddMuSLQQkTQ4IqFr77_G@babar.elephantsql.com:5432/dtsjjtzz')
+// Database connection
+const DBConnection = new Sequelize('postgres://dtsjjtzz:LQWdLNzmw7RTddMuSLQQkTQ4IqFr77_G@babar.elephantsql.com:5432/dtsjjtzz')
 
-// // Connection testing
-// DBConnection
-//     .authenticate()
-//     .then(() => {
-//       console.log('DB connection successful')
-//     })
-//     .catch((err) => {
-//       console.log('DB connection ERROR: ', err)
-//     })
+// Connection testing
+DBConnection
+    .authenticate()
+    .then(() => {
+      console.log('DB connection successful')
+    })
+    .catch((err) => {
+      console.log('DB connection ERROR: ', err)
+    })
 
-// module.exports = DBConnection
+module.exports = DBConnection
 
