75b084a622f3aed4d12a36f6f58ec4fb51cb2745
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-02-21 17:27:33 -0800

    commented out

--- server/api/hooks.js
+++ server/api/hooks.js
@@ -1,4 +1,4 @@
-const LectureCompletion = require('./model.js')
+// const LectureCompletion = require('./model.js')
 
 
 module.exports = {
@@ -9,17 +9,17 @@ module.exports = {
 	teachPost: function(req,res){
 		console.log('in post');
 		console.log(req.body);
-		if(req.body.type === "lectureprogress.created"){
-			LectureCompletion.create({
-				User_Name: req.body.object.user.name,
-				User_Email: req.body.object.user.email,
-				User_id: req.body.object.user.id,
-				Lecture_ID: req.body.object.lecture.id,
-				Lecture_Completion_Date: req.body.created,
-				Sign_In_Count: req.body.object.user.sign_in_count,
-				Last_Sign_In: req.body.object.user.last_sign_in_at
-			})
-		}
+		// if(req.body.type === "lectureprogress.created"){
+		// 	LectureCompletion.create({
+		// 		User_Name: req.body.object.user.name,
+		// 		User_Email: req.body.object.user.email,
+		// 		User_id: req.body.object.user.id,
+		// 		Lecture_ID: req.body.object.lecture.id,
+		// 		Lecture_Completion_Date: req.body.created,
+		// 		Sign_In_Count: req.body.object.user.sign_in_count,
+		// 		Last_Sign_In: req.body.object.user.last_sign_in_at
+		// 	})
+		// }
 		res.send(req.body);
 	}
 }
--- server/api/model.js
+++ server/api/model.js
@@ -1,19 +1,19 @@
-const Sequelize = require('sequelize');
-const db = require('./db.config.js');
+// const Sequelize = require('sequelize');
+// const db = require('./db.config.js');
 
-const LectureCompletion = db.define('completion', {
-	User_Name: Sequelize.STRING,
-	User_Email: Sequelize.STRING,
-	User_id: Sequelize.INTEGER,
-	Lecture_ID: Sequelize.INTEGER,
-	Lecture_Completion_Date: Sequelize.DATE,
-	Sign_In_Count: Sequelize.INTEGER,
-	Last_Sign_In: Sequelize.DATE
-})
+// const LectureCompletion = db.define('completion', {
+// 	User_Name: Sequelize.STRING,
+// 	User_Email: Sequelize.STRING,
+// 	User_id: Sequelize.INTEGER,
+// 	Lecture_ID: Sequelize.INTEGER,
+// 	Lecture_Completion_Date: Sequelize.DATE,
+// 	Sign_In_Count: Sequelize.INTEGER,
+// 	Last_Sign_In: Sequelize.DATE
+// })
 
 
-LectureCompletion.sync().then(() => {
-  console.log('Review table successfully created.')
-})
+// LectureCompletion.sync().then(() => {
+//   console.log('Review table successfully created.')
+// })
 
-module.exports = LectureCompletion
+// module.exports = LectureCompletion
