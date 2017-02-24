a35223d13f76546b0e7493260ba214ec68884d1b
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-02-24 14:02:16 -0800

    Reformat data to work with our data

--- server/api/hooks.js
+++ server/api/hooks.js
@@ -6,39 +6,35 @@ var fs = require('fs');
 module.exports = {
 	teachGet: function(req,res){
 		console.log('inside get');
-		var fields = ['car', 'price', 'color'];
-		var myCars = [
-		  {
-		    "car": "Audi",
-		    "price": 40000,
-		    "color": "blue"
-		  }, {
-		    "car": "BMW",
-		    "price": 35000,
-		    "color": "black"
-		  }, {
-		    "car": "Porsche",
-		    "price": 60000,
-		    "color": "green"
-		  }
-		];
-		var csv = json2csv({ data: myCars, fields: fields });
-		 
-		fs.writeFile('file.csv', csv, function(err) {
-		  if (err) throw err;
-		  console.log('file saved');
-		});
-		// LectureCompletion.findAll()
-		// 	.then((data) => {
-		// 		console.log(`data = ${data}`);
-		// 		data.forEach((row) => {
-		// 			console.log(`row.object = ${row.object}`);
-		// 		});
-		// 		res.json({"data": data});
-		// 	})
-		// 	.catch((err) => {
-		// 		throw err;
-		// 	});
+		// var fields = ['User Name', 'User Email', 'User ID', 'Lecture ID', 'Lecture Completion Date', 'Sign In Count', 'Last Sign In'];
+		// var sampleData = [
+		//   {
+		//     "User Name": "John Doe",
+		//     "User Email": "johnbto@icloud.csom",
+		//     "User ID": 3535791,
+		//     "Lecture ID": 12374767,
+		//     "Lecture Completion Date": "2017-02-24T19:10:30.000Z",
+		//     "Sign In Count": 15,
+		//     "Last Sign In": "2017-02-24T13:16:30.000Z"
+		//   }
+		// ];
+		// var csv = json2csv({ data: sampleData, fields: fields });
+		// fs.writeFile('file.csv', csv, function(err) {
+		//   if (err) throw err;
+		//   console.log('file saved');
+		// });
+
+		LectureCompletion.findAll()
+			.then((data) => {
+				console.log(`data = ${data}`);
+				data.forEach((row) => {
+					console.log(`row.object = ${row.object}`);
+				});
+				res.json({"data": data});
+			})
+			.catch((err) => {
+				throw err;
+			});
 	},
 	teachPost: function(req,res){
 		console.log('in post');
