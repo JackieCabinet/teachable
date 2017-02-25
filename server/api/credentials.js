47ca8ecc6cb38f0ec48bf165d57861cd2b13101f
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-02-24 17:11:53 -0800

    credentials file

--- server/api/credentials.js
+++ server/api/credentials.js
@@ -1,3 +1,3 @@
 module.exports = {
-	db: "postgres://dtsjjtzz:q00usl2X1H2ICB_I7vn5tTITABU4a-ip@babar.elephantsql.com:5432/dtsjjtzz'"
+	db: "postgres://dtsjjtzz:q00usl2X1H2ICB_I7vn5tTITABU4a-ip@babar.elephantsql.com:5432/dtsjjtzz"
 };
